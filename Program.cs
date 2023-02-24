3ed929d2795e32395838029cddc6e29a82f3bd9d
DmitriyK <d_kurban@yahoo.com>
2023-02-24 16:02:45 +1000

    edit ReadMe.md

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿string[] array = {"hello", "world", "hi", "Tom", "Jerry", "cat"};
-Console.WriteLine(array.Length);
-Console.WriteLine(array[3].Length);
-for (i = 0; i < array.Length; i++)
+﻿string[] array1 = {"hello", "world", "hi", "Tom", "Jerry", "cat"};
+Console.WriteLine(array1.Length);
+Console.WriteLine(array1[3].Length);
+int count = 0;
+for (i = 0; i < array1.Length; i++)
 {
-    if (array[i].Length <= 3)
+    if (array1[i].Length <= 3)
     {
-        Console.WriteLine(array[i]);
+        count ++;
+        Console.WriteLine(array1[i]);
     }
 }
+string[] array2 = new [count];
+int j = 0;
+for (i = 0; i < array1.Length; i++)
+{
+    if (array1[i] <= 3)
+    {
+        array2[j] = array1[i];
+        j ++; 
+    }
+}
+Console.WriteLine($"{String.Join(", ", array2)}");

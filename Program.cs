9ec216521c743f263ee9e6175fbde54de071e3d7
DmitriyK <d_kurban@yahoo.com>
2023-02-24 15:34:08 +1000

    added algoritm scheme

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿string[] array = {"hello", "world", "hi", "Tom", "Jerry", "cat"};
 Console.WriteLine(array.Length);
 Console.WriteLine(array[3].Length);
+for (i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
